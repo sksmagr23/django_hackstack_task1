023598f12588091dbc45a7fa426720332db8c8f6
sksmagr23 <sakshamag34@gmail.com>
2024-06-22 15:52:20 +0530

    created views and urls

--- moviedb/movies/views.py
+++ moviedb/movies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
 
-# Create your views here.
+def home(request):
+    movies = Movie.objects.all()
+    return render(request, 'movies/home.html', {'movies': movies})
+
+def movie_detail(request, pk):
+    movie = get_object_or_404(Movie, pk=pk)
+    return render(request, 'movies/movie_detail.html', {'movie': movie})
