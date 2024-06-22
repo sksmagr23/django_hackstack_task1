d1103c644f39507e6d709f01a229d7439c17b4ff
sksmagr23 <sakshamag34@gmail.com>
2024-06-22 16:39:34 +0530

    added the page templates and admin movie additions

--- moviedb/movies/views.py
+++ moviedb/movies/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
+from django.core.paginator import Paginator
 
 def home(request):
-    movies = Movie.objects.all()
-    return render(request, 'movies/home.html', {'movies': movies})
+    query = request.GET.get('q')
+    if query:
+        movies = Movie.objects.filter(title__icontains=query)
+    else:
+        movies = Movie.objects.all()
+    
+    paginator = Paginator(movies, 10)  # 10 movies per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    
+    return render(request, 'movies/home.html', {'page_obj': page_obj, 'query': query})
+
+
 
 def movie_detail(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
